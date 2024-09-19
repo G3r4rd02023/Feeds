a81c79613edee2f0153749396761584b64279b77
Gerardo Lanza <glanza007@gmail.com>
2024-09-19 10:42:16 -0600

    Show users

---
+++ Feeds/Feeds.Frontend/Controllers/UsuariosController.cs
@@ -0,0 +1,44 @@
+﻿using Feeds.Frontend.Services;
+using Feeds.Shared.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace Feeds.Frontend.Controllers
+{
+    [Authorize]
+    public class UsuariosController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IServicioUsuario _usuario;
+
+        public UsuariosController(IHttpClientFactory httpClientFactory, IServicioUsuario usuario)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7023/");
+            _usuario = usuario;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _usuario.GetUsuarioByEmail(User.Identity!.Name!);
+            var tokenService = new ServicioToken();
+            var token = await tokenService.Autenticar(user);
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.GetAsync("/api/Usuarios");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var usuarios = JsonConvert.DeserializeObject<IEnumerable<Usuario>>(content);
+                return View("Index", usuarios);
+            }
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            return View(new List<Usuario>());
+        }
+    }
+}
